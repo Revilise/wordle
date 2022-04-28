bd2a683b9daa3317d35dbe9ba1b93805f130e022
Revilise <nice.mutnykh@mail.ru>
2022-04-28 10:10:05 +0300

    encapsulate secret_word inside WordleProcessor

--- src/wordleProcessor/WordleProcessor.js
+++ src/wordleProcessor/WordleProcessor.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
-class WordleProcessor {
-    secret_word;
-    constructor() {}
 
-    GenerateRandomWord() {
+/*
+*   This module encapsulates secret_word variable.
+*   You can access to secret_word only by calling getSecret().
+ */
+
+function WordleProcessor() {
+    const getSecret = () => {
+        return this.secret_word;
+    }
+    const GenerateRandomWord = () => {
         const words = JSON.parse(fs.readFileSync('vocabulary.json'));
         const random_idx = Math.floor(Math.random() * words.length);
         this.secret_word = words[random_idx];
     }
-    CheckCorrectness(word) {
+    const CheckCorrectness = (word) => {
         if (word) {
             const word_chars = word.split('');
             const secretWord_chars = this.secret_word.split('');
 
             return word_chars.map((letter, idx) => {
-                const result = { letter };
+                const result = {letter};
                 if (secretWord_chars[idx] === letter) result.position = "right";
                 else if (secretWord_chars.includes(letter)) result.position = "exists";
                 else result.position = "none";
@@ -24,10 +30,11 @@ class WordleProcessor {
         }
         return [];
     }
-    CheckWordExistance(word) {
+    const CheckWordExistence = (word) => {
         const words = JSON.parse(fs.readFileSync('vocabulary.json'));
         return words.includes(word);
     }
+    return { getSecret, GenerateRandomWord, CheckCorrectness, CheckWordExistence }
 }
 
-export default const processor = new WordleProcessor();
+export default new WordleProcessor();
