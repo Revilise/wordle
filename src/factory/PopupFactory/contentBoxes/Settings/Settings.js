9fd1b4bfd0be4a3fb5326096f83016ebbcc2e1f8
Revilise <nice.mutnykh@mail.ru>
2022-07-09 21:20:37 +0300

    popup + settings

--- src/factory/PopupFactory/contentBoxes/Settings/Settings.js
+++ src/factory/PopupFactory/contentBoxes/Settings/Settings.js
@@ -1,7 +1,8 @@
-import './Settings.css';
+import classes from './Settings.module.scss';
 
 import {Difficulty} from "./Settings/Difficulty/Difficulty";
 import Switch from "../../../../features/Switch/Switch";
+import Item from "./SettingsItem";
 
 export default function Settings(props) {
     const {theme, difficulty} = props.app;
@@ -11,9 +12,24 @@ export default function Settings(props) {
         changeTheme(theme === "theme__light" ? "theme__dark" : "theme__light")
     }
     return (
-        <div className="settings-box">
-            <Switch checked={theme === "theme__dark"} handler={toggleTheme}/>
-            <Difficulty theme={theme} handler={changeDifficulty} difficulty={difficulty}/>
+        <div className={classes.settings}>
+            <Item>
+                <Item.Description label="Dark theme">
+                    Switch theme into dark or light.
+                </Item.Description>
+                <Item.Tool>
+                    <Switch checked={theme === "theme__dark"} handler={toggleTheme}/>
+                </Item.Tool>
+            </Item>
+            <Item>
+                <Item.Description label="Difficulty">
+                    How many tries do you need to guess the secret word.
+                </Item.Description>
+                <Item.Tool>
+                    <Difficulty theme={theme} handler={changeDifficulty} difficulty={difficulty}/>
+                </Item.Tool>
+            </Item>
         </div>
     )
 }
+
--- src/features/Popup/Popup.js
+++ src/features/Popup/Popup.js
@@ -2,17 +2,36 @@ import classes from './Popup.module.scss'
 
 import closeLight from '../../assets/light-theme/close-light.svg';
 import closeDark from '../../assets/dark-theme/close-dark.svg';
+import {createRef, useEffect} from "react";
 
 export function Popup({theme, onClick, content, title}) {
+    const popup = createRef()
+    const back = createRef()
+
+    useEffect(() => {
+        // даю время стилям загрузиться.
+        setTimeout(() => {
+            back.current.style.opacity = 1;
+            popup.current.style.height = popup.current.scrollHeight + "px"
+        }, 0)
+    },[])
+
+    function ClosePopup() {
+        back.current.style.opacity = 0;
+        popup.current.style.height = "0";
+        setTimeout(onClick, 200)
+    }
     return (
-        <div className={classes.popup_container}>
-            <div className={classes.popup}>
+        <div ref={back} className={classes.popup_back}>
+            <div ref={popup} className={classes.popup}>
                 <div className={classes.popup_close}>
-                    <input onClick={onClick} type="image"
+                    <input onClick={ClosePopup} type="image"
                            src={theme === "theme__light" ? closeLight : closeDark} alt=""/>
                 </div>
-                <h2 className={classes.popup_title}>{title}</h2>
-                {content}
+                <div className={classes.popup_content}>
+                    <h2 className={classes.popup_title}>{title}</h2>
+                    {content}
+                </div>
             </div>
         </div>
     )
